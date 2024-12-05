496e5c9b9c3249f1fdf5a767fad70f9051ff28a6
lior <lior277@gmail.com>
2024-12-05 11:01:05 +0200

    o

--- tests/test_add_to_chart_new.py
+++ tests/test_add_to_chart_new.py
@@ -3,7 +3,7 @@ from Infrastructure.objects.objects_api.product_page_api import ProductPageApi
 from Infrastructure.Infra.dal.api_access.api_accsess import ApiAccess
 
 
-class TestSecond:
+class TestSecondNew:
     url = "https://api.demoblaze.com/addtocart"
 
     @pytest.mark.regression
@@ -15,4 +15,4 @@ class TestSecond:
         product_page_api = ProductPageApi(api_access=api_access)
 
         # Step 3: Call the add_item_to_chart method
-        product_page_api.add_item_to_chart(TestSecond.url)
+        product_page_api.add_item_to_chart(TestSecondNew.url)
