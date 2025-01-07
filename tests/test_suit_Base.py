0636011e69b212607e98f2f16178e68625d8892e
lior <lior277@gmail.com>
2025-01-07 14:40:46 +0200

    fix

--- tests/test_suit_Base.py
+++ tests/test_suit_Base.py
@@ -14,38 +14,32 @@ class TestSuitBase:
 
         if is_debug:
             # Disable headless mode for debugging (GUI mode)
-            print("Running in debug mode - Chrome GUI will open. ")
+            print("Running in debug mode - Chrome GUI will open.")
             chrome_options.add_argument('--no-sandbox')  # Needed for some systems in Docker
-            chrome_options.add_argument('--lang=en-GB')
-            chrome_options.add_argument('--accept-language=en-US,en;q=0.9')
             chrome_options.add_argument('--disable-dev-shm-usage')  # Docker-related fix
             # No need for headless in debug mode
-            chrome_options.add_argument('window-size=1920x1080')  # Fixed window size for debugging
         else:
             # Run in headless mode for non-debug runs (ideal for CI/CD)
             print("Running in headless mode - No GUI.")
-            chrome_options.add_argument('--lang=en-GB')
-            chrome_options.add_argument('--accept-language=en-US,en;q=0.9')
             chrome_options.add_argument('--headless')  # Chrome will run in headless mode
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.add_argument('--disable-browser-side-navigation')
-            chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
+            chrome_options.add_argument('--no-sandbox')  # Needed for some systems in Docker
+            chrome_options.add_argument('--disable-dev-shm-usage')  # Docker-related fix
 
-        # Add common options
-        chrome_options.add_argument('--remote-debugging-port=9222')  # Enable debugging
-
-        # Set up the ChromeDriver with ChromeDriverManager
-        driver = webdriver.Chrome(
-            service=ChromeService(ChromeDriverManager().install()),
+        # Set up the WebDriver to use Selenium Grid (point to the Hub)
+        driver = webdriver.Remote(
+            command_executor='http://selenium-hub:4444/wd/hub',
             options=chrome_options
         )
 
-        driver.maximize_window()  # Ensure the window is maximized in debug mode
+        # Optionally, you can specify window size for non-headless mode
+        if is_debug:
+            chrome_options.add_argument('window-size=1920x1080')  # Fixed window size for debugging
+
         return driver
 
     @staticmethod
     def driver_dispose(driver: webdriver = None):
-        if driver:
-            print("Teardown: Closing the browser.")
-            driver.quit()  # This will stop the session and close the browser
+        if driver is not None:
+            driver.close()
+        if driver is not None:
+            driver.quit()
